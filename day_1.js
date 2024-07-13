abfbac6340a046e0f4f5bda3031c18271b65e7d1
Abhishek120797 <abhi.jais1211@gmail.com>
2024-07-13 16:08:59 +0530

    day 1 completed

---
+++ day_1.js
@@ -0,0 +1,54 @@
+//Day 1: Variable and data type
+
+// Activity 1:Variable declaration
+
+// Task 1:
+
+var num = 10;
+console.log(num);
+
+// Task 2:
+
+let str = "Abhishek Jaiswal";
+console.log(str);
+
+// Activity 2: Constant declaration
+
+// Task 3:
+
+const bool_val = true;
+console.log(bool_val);
+
+// Activity 3: Data Types
+
+// Task 4:
+
+let number = 10;
+let string = "Jaiswal";
+let bool_value = false;
+const obj = { name: "Abhishek", age: 27, gender: "male" };
+const arr = [1, 2, 3, 4, 5];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof bool_value);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity 4: Reassigning Variable
+
+// Task 5:
+
+let some_var = 100;
+console.log(some_var);
+some_var = 200;
+console.log(some_var);
+
+// Activity 5: Understanding const
+
+// Task 6:
+
+const const_var = 300;
+console.log(const_var);
+const_var = 500;
+console.log(const_var);

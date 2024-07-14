3c31377053af516d14995dada33e19e3311dd7d8
Abhishek120797 <abhi.jais1211@gmail.com>
2024-07-14 18:11:27 +0530

    Day 2 challenge completed

---
+++ day_2.js
@@ -0,0 +1,72 @@
+//Day 2: Operators
+
+// Activity 1: Arithmetic Operations
+
+// Task 1:
+
+console.log(10 + 5);
+
+// Task 2:
+
+console.log(10 - 5);
+
+// Task 3:
+
+console.log(10 * 5);
+
+// Task 4:
+
+console.log(10 / 5);
+
+// Task 5:
+
+console.log(21 % 5);
+
+// Activity 2: Assignment Operators
+
+// Task 6:
+
+let num = 5;
+num += 10;
+console.log(num);
+
+// Task 7:
+
+let num_2 = 20;
+num_2 -= 5;
+console.log(num_2);
+
+// Activity 3: Comparison Operators
+
+// Task 8:
+
+console.log(10 > 5);
+
+// Task 9:
+
+console.log(10 < 5);
+
+// Task 10:
+
+console.log(10 == 10);
+console.log(10 === 10);
+
+// Activity 4: Logical Operators
+
+// Task 11:
+
+console.log(10 > 5 && 4 < 10);
+
+// Task 12:
+
+console.log(10 > 5 || 20 < 10);
+
+// Task 13:
+
+console.log(!10 == 5);
+
+// Activity 5: Ternary Operators
+
+// Task 14:
+
+10 < 0 ? console.log("Number is Negative") : console.log("Number is positive");
